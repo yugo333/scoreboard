a90f8b7470a22da88e88998eeb5c59dd263ac969
yugo333 <yugo199103@gmail.com>
2020-04-16 01:20:12 +0900

    テスト

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 {
-  const pointsA = document.getElementById("pointsA");
+  const pointsA = document.getElementById("pointsA"); //
   const pointsB = document.getElementById("pointsB");
   const plusA = document.getElementById("plusA");
   const minusA = document.getElementById("minusA");
