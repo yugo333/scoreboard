c7736732ea6c4e1d4487b0ab996b516f2d86ceec
yugo333 <yugo199103@gmail.com>
2020-04-16 12:08:23 +0900

    Revert "削除iranai"
    This reverts commit 70d50616ddac086dbbec0892f5b58ecb757be520.

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@
 "use strict";
 {
   const pointsA = document.getElementById("pointsA"); //
+  const pointsB = document.getElementById("pointsB");
   const plusA = document.getElementById("plusA");
   const minusA = document.getElementById("minusA");
-
+  const plusB = document.getElementById("plusB");
+  const minusB = document.getElementById("minusB");
 
 
 
